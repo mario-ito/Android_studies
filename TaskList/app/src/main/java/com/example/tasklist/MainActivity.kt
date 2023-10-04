41acd2ce51b00a247d17235e3a147951a99977dd
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-04 20:34:49 -0300

    wip tasklist

---
+++ TaskList/app/src/main/java/com/example/tasklist/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.tasklist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("DIALOG BOX")
+        builder.create()
+    }
+}
